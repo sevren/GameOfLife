0c2c00c08ce978f97bc639eb011a08b53fca62db
Kiril Goguev <kgoguev@gmail.com>
2015-01-21 03:00:57 +0100

    added C# game of life

---
+++ C#/GameOfLife/GOL.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GameOfLife
+{
+    partial class GOL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(-1, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(-1, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.Location = new System.Drawing.Point(81, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Game of Life C#-edition";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ C#/GameOfLife/GOL.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    
+    public partial class GOL : Form
+    {
+        private bool[,] board;
+        private Label[,] cells;
+
+        private bool[,] initIntBoard(bool[,] b,int r, int c)
+        {
+	        for (int i=0; i<r;i++)
+	        {
+		        for (int j=0;j<c;j++)
+		        {
+			        b[i,j]=false;
+		        }
+	        }
+	
+	        return b;
+        }
+
+        private void translateToGui(bool[,] newboard2)
+        {
+            //get # of rows
+            for (int i = 0; i < newboard2.GetLength(0); i++)
+            {
+                //get # of cols
+                for (int j = 0; j < newboard2.GetLength(1); j++)
+                {
+                    if (!newboard2[i,j])
+                    {
+                        cells[i,j].BackColor=Color.Gray;
+                    }
+                    else
+                    {
+                        cells[i,j].BackColor=Color.Yellow;
+                    }
+                }
+            }
+        }
+
+    public bool[,] simulate(bool[,] board)
+    {
+	    //neighbours counter for each square
+	    int n=0;
+	    //complete waste of memory for now
+	    bool[,] newboard=new bool[board.GetLength(0),board.GetLength(1)];
+
+	
+	    //get # of rows
+	    for (int i=0; i<board.GetLength(0);i++)
+	    {
+		    //get # of cols
+		    for (int j=0;j<board.GetLength(1);j++)
+		    {
+			    n=0;
+			
+			    //
+			     // Abuse of the Exceptions for now  :( a better method would be calculating the edges
+			     //
+			
+			    //top-left check (r-1,c-1)
+			    try
+			    {
+				    if (board[i-1,j-1])
+					    n++;
+			    }
+			    catch(Exception e)
+			    {
+				
+			    }
+			
+			    //top check (r-1,c)
+			    try
+			    {
+				    if (board[i-1,j])
+					    n++;
+			    }
+			    catch(Exception e)
+			    {
+				
+			    }
+			
+			    //top-right check (r-1,c+1)
+			    try
+			    {
+				    if (board[i-1,j+1])
+					    n++;
+			    }
+			    catch(Exception e)
+			    {
+				
+			    }
+			
+			    //right check (r,c+1)
+			    try
+			    {
+				    if (board[i,j+1])
+					    n++;
+			    }
+			    catch(Exception e)
+			    {
+				
+			    }
+			
+			    //bottom-right check (r+1,c+1)
+			    try
+			    {
+				    if (board[i+1,j+1])
+					    n++;
+			    }
+			    catch(Exception e)
+			    {
+				
+			    }
+			
+			    //bottom check (r+1,c)
+			    try
+			    {
+				    if (board[i+1,j])
+					    n++;
+			    }
+			    catch(Exception e)
+			    {
+				
+			    }
+			
+			    //bottom-Left check (r+1,c-1)
+			    try
+			    {
+				    if (board[i+1,j-1])
+					    n++;
+			    }
+			    catch(Exception e)
+			    {
+				
+			    }
+			
+			    //left check (r,c-1)
+			    try
+			    {
+				    if (board[i,j-1])
+					    n++;
+			    }
+			    catch(Exception e)
+			    {
+				
+			    }
+			
+			    // now apply rules
+			
+			    //loney
+			    if ((board[i,j]) && (n <=1))
+			    {
+				    newboard[i,j]=false;
+			    }
+			
+			    //Overcrowded
+			    if ((board[i,j]) && (n >=4))
+			    {
+				    newboard[i,j]=false;
+			    }
+			
+			    //Continue Living
+			    if ((board[i,j]) && (n ==2 || n == 3))
+			    {
+				    newboard[i,j]=true;
+			    }
+			
+			    //Birth
+			    if ((!board[i,j]) && (n ==3))
+			    {
+				    newboard[i,j]=true;
+			    }
+			
+			    //Barren
+			    if ((!board[i,j]) && (n !=3))
+			    {
+				    newboard[i,j]=false;
+			    }
+			
+		
+		    }
+	    }
+	
+	    return newboard;
+	
+    }
+
+
+        private void GenerateTable(int rowCount, int columnCount)
+        {
+            //Clear out the existing controls, we are generating a new table layout
+            tableLayoutPanel1.Controls.Clear();
+
+            //Clear out the existing row and column styles
+            tableLayoutPanel1.ColumnStyles.Clear();
+            tableLayoutPanel1.RowStyles.Clear();
+
+            //Now we will generate the table, setting up the row and column counts first
+            tableLayoutPanel1.ColumnCount = columnCount;
+            tableLayoutPanel1.RowCount = rowCount;
+
+            for (int x = 0; x < rowCount; x++)
+            {
+                //First add a column
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+
+                for (int y = 0; y < columnCount; y++)
+                {
+                    //Next, add a row.  Only do this when once, when creating the first column
+                    if (x == 0)
+                    {
+                        tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+                    }
+
+                    //Create the control, in this case we will add a label
+                    Label lbl = new Label();
+                    
+                    lbl.BackColor = Color.Gray;
+                    lbl.BorderStyle = BorderStyle.Fixed3D;
+                    lbl.Size = new System.Drawing.Size(20, 20);
+                    lbl.Click += new System.EventHandler(this.label_onClick);
+                    lbl.Name = x + "," + y;
+                    lbl.BackColorChanged += new System.EventHandler(this.label_colorChanged);
+                    cells[x, y] = lbl;
+
+                    //Finally, add the control to the correct location in the table
+                    tableLayoutPanel1.Controls.Add(cells[x,y], y, x);
+                }
+            }
+        }
+
+        void lbl_BackColorChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public GOL()
+        {
+            int r=15;
+            int c=25;
+            board = new bool[r, c];
+            cells = new Label[r, c];
+            InitializeComponent();
+            GenerateTable(r, c);
+            
+            board=initIntBoard(board, r, c);
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label_onClick(object sender, EventArgs e)
+        {
+            //cast the object as a Label;
+            Label lbl = (Label)sender;
+            //split on the ',' in the name of the label =>tokens[0] is row and tokens[1] is column
+            string[] tokens = lbl.Name.Split(new string[] { "," }, StringSplitOptions.None);
+           
+
+            if (lbl.BackColor == Color.Gray)
+            {
+                lbl.BackColor = Color.Yellow;
+                board[Convert.ToInt32(tokens[0]),Convert.ToInt32(tokens[1])] = true;
+
+            }
+            else
+            {
+                lbl.BackColor = Color.Gray;
+                board[Convert.ToInt32(tokens[0]),Convert.ToInt32(tokens[1])] = false;
+
+            }
+            
+
+        }
+
+        private void label_colorChanged(object sender, EventArgs e)
+        {
+            //cast the object as a Label
+           // Label lbl = (Label)sender;
+            //split on the ',' in the name of the label =>tokens[0] is row and tokens[1] is column
+            //string[] tokens = lbl.Name.Split(new string[] { "," }, StringSplitOptions.None);
+            //Console.WriteLine("Label "+ lbl.Name+" has changed color!");
+            //Console.WriteLine("Tokens are " + tokens[0] +" and "+tokens[1]);
+            
+            //board[Convert.ToInt32(tokens[0]),Convert.ToInt32( tokens[1])] = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            board = simulate(board);
+            translateToGui(board);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+       
+    }
+}
